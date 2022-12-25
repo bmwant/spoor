24710043f4388a0460f2b1e1c2f44dab49741ed0
Misha Behersky <bmwant@gmail.com>
2022-12-25 17:34:43 +0100

    test: add tests package with pytest :evergreen_tree:

--- spoor/__init__.py
+++ spoor/__init__.py
@@ -1,20 +1,42 @@
 from functools import wraps
+from collections import Counter
 
 class Spoor:
-    def __call__(self, func):
+    def __init__(self, attach: bool = False):
+        self.attach = attach
+        # TODO: add lock for thread safety
+        self._registry = Counter()
+    
+    def track(self, func):
         @wraps(func)
         def inner(*args, **kwargs):
-            print("Wrapping", func)
+            func_hash = self._get_hash(inner)
+            self._registry[func_hash] += 1
             return func(*args, **kwargs)
         return inner
 
+    def _get_hash(self, func_id):
+        return hash(func_id)
+
+    def called(self, func_id) -> bool:
+        return self.call_count(func_id) != 0
+
+    def call_count(self, func_id):
+        func_hash = self._get_hash(func_id)
+        return self._registry[func_hash]
 
 
-s = Spoor()
 
-@s
-def my_func():
-    print("my_func is called")
+def main():
+    s = Spoor()
+
+    @s.track
+    def my_func():
+        print("my_func is called")
+
+    my_func()
+    print()
 
 
-my_func()
+if __name__ == "__main__":
+    main()
---
+++ tests/test_functions.py
@@ -0,0 +1,37 @@
+from spoor import Spoor
+
+
+def test_call_count():
+    s = Spoor()
+
+    @s.track
+    def target():
+        pass
+
+    target()
+    assert s.call_count(target) == 1
+
+    target()
+    assert s.call_count(target) == 2
+
+
+def test_called():
+    s = Spoor()
+
+    @s.track
+    def target():
+        pass
+
+    target()
+
+    assert s.called(target) is True
+
+
+def test_not_called():
+    s = Spoor()
+
+    @s.track
+    def target():
+        pass
+
+    assert s.called(target) is False
