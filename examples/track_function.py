9445feefa9fd1961b17e9d48836eabbb0b2dcbc5
Misha Behersky <bmwant@gmail.com>
2023-01-01 15:35:22 +0100

    docs: add example to track functions :pouch:

---
+++ examples/track_function.py
@@ -0,0 +1,35 @@
+from spoor import Spoor
+
+s = Spoor()
+
+
+@s.track
+def track_first(a: int, b: int) -> int:
+    return a + b
+
+
+@s.track
+def track_second(a: float, b: float) -> float:
+    return a / b
+
+
+@s.track
+def track_third():
+    return {}
+
+
+if __name__ == "__main__":
+    # given
+    track_first(2, 3)
+    track_second(1.0, 5.0)
+    track_second(2.1, 3.2)
+
+    # when
+    assert s.called(track_first)
+    assert s.called(track_second)
+    assert not s.called(track_third)
+
+    # then
+    assert s.call_count(track_first) == 1
+    assert s.call_count(track_second) == 2
+    assert s.call_count(track_third) == 0
