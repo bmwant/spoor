492eba401c1fc54c2fe31f7008a5210f8f8ae9ed
Misha Behersky <bmwant@gmail.com>
2023-01-07 16:51:57 +0100

    test: add tests for distinct and groupped instances hashes :grey_exclamation:

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -34,8 +34,52 @@ def test_hash_unbound_method(distinct_instances):
 
 
 def test_hash_disctinct_instances():
-    pass
+    s = Spoor(distinct_instances=True)
+
+    class Target:
+        def method(self):
+            pass
+
+    original = Target.method
+    Target = s.track(Target)
+
+    t1 = Target()
+    t2 = Target()
+
+    assert id(t1.method) != id(original)
+    assert hash(t1.method) != hash(original)
+    assert t1.method._func is original
+
+    assert id(t2.method) != id(original)
+    assert hash(t2.method) != hash(original)
+    assert t2.method._func is original
+
+    assert id(t1.method) != id(t2.method)  # different objects
+    assert hash(t1.method) != hash(t2.method)  # different hashes
+    assert t1.method._func is t2.method._func  # same underlying function
 
 
 def test_hash_groupped_instances():
-    pass
+    s = Spoor(distinct_instances=False)
+
+    class Target:
+        def method(self):
+            pass
+
+    original = Target.method
+    Target = s.track(Target)
+
+    t1 = Target()
+    t2 = Target()
+
+    assert id(t1.method) != id(original)  # different objects
+    assert hash(t1.method) == hash(original)  # but same hash to track
+    assert t1.method._func is original
+
+    assert id(t2.method) != id(original)
+    assert hash(t2.method) == hash(original)
+    assert t2.method._func is original
+
+    assert id(t1.method) != id(t2.method)  # different bound methods
+    assert hash(t1.method) == hash(t2.method)  # but same hash when groupped
+    assert t1.method._func is t2.method._func  # same underlying function
